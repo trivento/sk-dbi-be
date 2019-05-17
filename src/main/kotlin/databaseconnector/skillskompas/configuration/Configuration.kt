85e5450ded82d17243968356b5b9aff226e6228a
Yvan Stemmerik <y.stemmerik@student.vu.nl>
2019-05-17 11:12:18 +0200

    Master -> auth0 branch

--- src/main/kotlin/databaseconnector/skillskompas/configuration/Configuration.kt
+++ src/main/kotlin/databaseconnector/skillskompas/configuration/Configuration.kt
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+
+
 
 
 @Configuration
@@ -13,4 +16,15 @@ class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
     }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/")
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+
 }
+
